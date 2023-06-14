cbcca22883152c350823f0dc4b62374d94861a77
johirjoy <joy.sd69@gmail.com>
2023-06-14 20:46:38 +0600

    student payment history set

--- src/pages/Dashboard/EnrolledClass/EnrolledClass.jsx
+++ src/pages/Dashboard/EnrolledClass/EnrolledClass.jsx
@@ -1,9 +1,15 @@
+import { Helmet } from "react-helmet";
 
 
 const EnrolledClass = () => {
     return (
-        <div className="bg-slate-300 p-10">
-            <h3>This is Enrolled class</h3>
+        <div className="bg-slate-300 w-[600px] p-10">
+            <Helmet>
+                <title>JSSA | Enrolled Classes</title>
+            </Helmet>
+            <div className="text-center w-3/4 mb-10 mx-auto space-y-3">
+                <h1 className="md:text-3xl text-slate-600 font-bold uppercase border-y-4 border-slate-400 py-4 ">Enrolled Class</h1>
+            </div>
         </div>
     );
 };
--- src/pages/Dashboard/Payment/PaymentHistory.jsx
+++ src/pages/Dashboard/Payment/PaymentHistory.jsx
@@ -1,9 +1,16 @@
-
+import { Helmet } from "react-helmet";
 
 const PaymentHistory = () => {
+
+
     return (
-        <div className="bg-slate-300 p-10">
-            <h3>This is payment history</h3>
+        <div className="bg-slate-300 w-[600px] p-10">
+            <Helmet>
+                <title>JSSA | Payment History</title>
+            </Helmet>
+            <div className="text-center w-3/4 mb-10 mx-auto space-y-3">
+                <h1 className="md:text-3xl text-slate-600 font-bold uppercase border-y-4 border-slate-400 py-4 ">Payment History</h1>
+            </div>
         </div>
     );
 };
